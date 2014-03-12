361277f3fb2c587bc5f21919feb0d5045fc8f92c
Carlos Martinez <ctmartinez1992@gmail.com>
2014-03-12 13:32:38 +0000

    Added a version of the code written in lolcode

--- src/potd/Day6_24HClock.java
+++ src/potd/Day6_24HClock.java
@@ -1,6 +1,39 @@
 package potd;
 
 /**
+ * Clock Hands
+ * Write a program to find the minimum angle between two hands on a 24 hour clock.
+ * For instance, the angle at 6:00 is 90 degrees and the angle at 18:00 is also 90 degrees.
+ * At 6:17 the degree is 3.5 and at 18:17 it's 176.5 (hooray for supplementary angles).
+ * 
+ * For fun, program this one up in a language you've never used before.
+ * Here is a list of esoteric languages to help you decide.
+ * There are some truly interesting languages on that list.
+ * Feel free to use a "standard" language as well if there's one out there that you've been looking to learn.
+ * 
+ * 
+ * In LOLCODE
+ * 
+ * HAI
+ *  I HAS A HOUR
+ *  I HAS A MINUTE
+ *  GIMMEH HOUR
+ *  GIMMEH MINUTE
+ *  I HAS A TMP ITZ QUOSHUNT OF MINUTE AN 60
+ *  I HAS A HOURTMP ITZ SUM OF HOUR AN TMP
+ *  I HAS A HANGLE ITZ PRODUKT OF 360 AN QUOSHUNT OF HOURTMP AN 24
+ *  I HAS A MANGLE ITZ DIFF OF TMP AN 360
+ *  I HAS A DIFFE ITZ DIFF OF HANGLE AN MANGLE
+ * 
+ *  BIGGR OF DIFFE AN 180, O RLY?
+ *   YA RLY
+ *    I HAS A NDIFF ITZ DIFF OF 360 AN DIFFE
+ *    VISIBLE NDIFF
+ *   NO WAI
+ *  VISIBLE DIFFE
+ *  OIC
+ * KTHXBYE
+ * 
  *
  * @author Carlos
  */
--- src/potd/POTD.java
+++ src/potd/POTD.java
@@ -4,20 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Clock Hands
- * Write a program to find the minimum angle between two hands on a 24 hour clock.
- * For instance, the angle at 6:00 is 90 degrees and the angle at 18:00 is also 90 degrees.
- * At 6:17 the degree is 3.5 and at 18:17 it's 176.5 (hooray for supplementary angles).
- * 
- * For fun, program this one up in a language you've never used before.
- * Here is a list of esoteric languages to help you decide.
- * There are some truly interesting languages on that list.
- * Feel free to use a "standard" language as well if there's one out there that you've been looking to learn.
- * 
- *
- * @author Carlos
- */
 public class POTD {
     
     public static void main(String[] args) throws IOException {
